468a58e76a680a82eb9ba2af51511ea5a4d9fe12
PawelPlutaUek20 <pawelpluta58@gmail.com>
2023-08-20 13:30:28 +0200

    Rate Limiting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-import { type Component, createResource } from 'solid-js';
+import { type Component, createResource } from "solid-js";
 
 const showMovieHandler = async () =>
   (await fetch(`http://localhost:4000/v1/movies/1`)).json();
 
+const testRateLimit = async () => {
+  return Promise.allSettled(
+    Array(6)
+      .fill(undefined)
+      .map(() =>
+        fetch("http://localhost:4000/v1/healthcheck").then((res) => {
+          if (!res.ok) {
+            return res.json().then((err) => {
+              throw err;
+            });
+          }
+          return res.json();
+        })
+      )
+  );
+};
 
 const App: Component = () => {
   const [resource] = createResource(showMovieHandler);
@@ -10,6 +26,7 @@ const App: Component = () => {
   return (
     <div>
       <span>{resource.loading && "Loading..."}</span>
+      <button onClick={() => testRateLimit().then(console.log)}>onClick</button>
       <div style={{ margin: "0 1rem" }}>
         <pre>{JSON.stringify(resource(), null, 2)}</pre>
       </div>
--- server/go.mod
+++ server/go.mod
@@ -2,10 +2,10 @@ module lets-go-further.pawelplutauek20.pl
 
 go 1.20
 
-require github.com/julienschmidt/httprouter v1.3.0
-
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
 	github.com/rs/cors v1.9.0
+	golang.org/x/time v0.3.0
 )
